0452e9fddaa6c2d15fbfe75c01d38e6e0d47ac2a
Daniel Berkompas <himself@danielberkompas.com>
2016-09-08 17:35:52 -0700

    Bump dependencies

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule Cloak.Mixfile do
     [{:poison, ">= 1.5.0"},
      {:ecto, ">= 1.0.0"},
      {:ex_doc, "~> 0.11", only: :docs},
-     {:inch_ex, only: :docs}
+     {:inch_ex, ">= 0.0.0", only: :docs}
    ]
   end
 
