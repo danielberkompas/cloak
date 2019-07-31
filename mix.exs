8e1f9bd06882a598770cbf593228cf430ac85b1e
Daniel Berkompas <himself@danielberkompas.com>
2019-07-31 15:53:15 -0700

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cloak.Mixfile do
   def project do
     [
       app: :cloak,
-      version: "1.0.0-alpha.0",
+      version: "1.0.0",
       elixir: "~> 1.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
