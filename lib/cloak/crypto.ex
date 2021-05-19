4b2e773eac178430684f26d563b729fd219272e6
Michael Quinlan <mquinlan@gigsmart.com>
2021-05-19 09:56:49 -0600

    Ensuring backward compatibility

--- lib/cloak/crypto.ex
+++ lib/cloak/crypto.ex
@@ -10,7 +10,7 @@ defmodule Cloak.Crypto do
     :crypto.strong_rand_bytes(num)
   end
 
-  if System.otp_release() >= "22" do
+  if System.otp_release() >= "24" do
     def encrypt_one_time(cipher, key, iv, plaintext) do
       :crypto.crypto_one_time(cipher, key, iv, plaintext, encrypt: true)
     end
@@ -25,6 +25,26 @@ defmodule Cloak.Crypto do
 
     def decrypt_one_time_aead(cipher, key, iv, aad, ciphertext, ciphertag) do
       :crypto.crypto_one_time_aead(cipher, key, iv, aad, ciphertext, ciphertag, false)
+    end
+  else
+    def encrypt_one_time(cipher, key, iv, plaintext) do
+      state = :crypto.stream_init(cipher, key, iv)
+      {_state, ciphertext} = :crypto.stream_encrypt(state, plaintext)
+      ciphertext
+    end
+
+    def decrypt_one_time(cipher, key, iv, ciphertext) do
+      state = :crypto.stream_init(cipher, key, iv)
+      {_state, plaintext} = :crypto.stream_decrypt(state, ciphertext)
+      plaintext
+    end
+
+    def encrypt_one_time_aead(cipher, key, iv, aad, plaintext) do
+      :crypto.block_encrypt(cipher, key, iv, {aad, plaintext})
+    end
+
+    def decrypt_one_time_aead(cipher, key, iv, aad, ciphertext, ciphertag) do
+      :crypto.block_decrypt(cipher, key, iv, {aad, ciphertext, ciphertag})
     end
   end
 end
