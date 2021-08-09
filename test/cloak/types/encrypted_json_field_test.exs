2e198c406b33d8859fe08962593c03287fbd5372
minsoju <kimsmindy@gmail.com>
2021-08-09 14:33:10 -0600

    Add test

---
+++ test/cloak/types/encrypted_json_field_test.exs
@@ -0,0 +1,18 @@
+defmodule Cloak.EncryptedJsonFieldTest do
+  use ExUnit.Case
+  alias Cloak.EncryptedJsonField, as: Field
+
+  test ".type is :binary" do
+    assert Field.type() == :binary
+  end
+
+  test ".dump encrypts the value" do
+    {:ok, ciphertext} = Field.dump("value")
+    assert ciphertext != "value"
+  end
+
+  test ".load decrypts the ciphertext" do
+    {:ok, ciphertext} = Field.dump("value")
+    assert {:ok, "value"} = Field.load(ciphertext)
+  end
+end
