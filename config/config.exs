7eb1652f422b1bfb38d0cf8aaacc0f346f55180e
Daniel Berkompas <himself@danielberkompas.com>
2022-06-17 06:40:00 -0700

    :arrow_up: Use new config format
    Instead of using Mix.Config

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
-if Mix.env() == :test do
+if config_env() == :test do
   import_config "test.exs"
 end
